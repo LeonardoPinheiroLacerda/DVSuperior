ea7ecec41e5261c98b08441fe891f11c1aa03268
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2021-05-07 21:01:48 -0300

    BASE_URL correction

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
+//Verifica se a variavel de anbiente BACKEND_URL existe no netlify (anbiente de deploy), se não existir 
+//significa que é um projeto local e o valor será o localhost
+
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
+export const BASE_URL = 'https://sds3-leonardo-lacerda.herokuapp.com';
